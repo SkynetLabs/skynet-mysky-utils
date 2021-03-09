7c7f859424a3661887da41afdb44a328759b43c5
Marcin S <marcin@bytedude.com>
2021-03-09 12:28:26 +0100

    Add common utils

---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+/**
+ * Creates an invisible iframe with the given src and adds it to the page.
+ */
+export function createIframe(srcUrl: string, name: string): HTMLIFrameElement {
+  srcUrl = ensureUrl(srcUrl);
+
+  const childFrame = document.createElement("iframe")!;
+  childFrame.src = srcUrl;
+  childFrame.name = name;
+  childFrame.style.display = "none";
+
+  // Set sandbox permissions.
+  // TODO: Enable sandboxing?
+  // childFrame.sandbox.add("allow-same-origin");
+  // childFrame.sandbox.add("allow-scripts");
+
+  document.body.appendChild(childFrame);
+  return childFrame;
+}
+
+function ensureUrl(url: string): string {
+  return ensurePrefix(url, "https://");
+}
+
+function ensurePrefix(s: string, prefix: string): string {
+  if (!s.startsWith(prefix)) {
+    s = `${prefix}${s}`;
+  }
+  return s;
+}
